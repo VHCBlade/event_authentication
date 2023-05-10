56e3f8e4c250eae17326a6bcccfe1704efcf704e
Vincent Haron C. Mamutuk <vhcblade@gmail.com>
2023-05-10 18:32:14 +0800

    Finished initial generator implementation

---
+++ lib/event_authenticator_db.dart
@@ -0,0 +1,5 @@
+/// Creates and authenticates user passswords
+library event_authenticator_db;
+
+export 'event_authenticator.dart';
+export 'src/authenticator/db/user.dart';
---
+++ test/authenticator/generator_test.dart
@@ -0,0 +1,9 @@
+import 'package:test/scaffolding.dart';
+
+void main() {
+  group('UserAuthenticationGenerator', () {
+    group('generateAuthentication', generateAuthenticationTest);
+  });
+}
+
+void generateAuthenticationTest() {}
